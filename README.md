7770c5d5c74a619fe99f34c0c4996ac961411020
Liza <obs4545@yandex.ru>
2023-12-10 03:24:28 +0300

    add selenoid

--- README.md
+++ README.md
@@ -19,10 +19,23 @@ pytest --browser=opera --headless
 
 --url="http://localhost:80"  (еще можно подставить айпи адрес вместо localhost)
 
-запуск
-pytest --url="http://localhost:80" 
-pytest tests/test_with_page_object.py --url="http://localhost:80" --log_level=DEBUG 
-pytest tests/test_find_elements.py --url="http://localhost:80" --log_level=INFO    
+ЗАПУСК НА ЛОКАЛЬНОЙ МАШИНЕ: запустить докер в докер-десктоп
+опенкарт находится по адресу http://localhost:80
+pytest tests/test_with_page_object.py  --log_level=DEBUG 
+pytest tests/test_find_elements.py  --log_level=INFO    
 запустили тесты, потом аллюр
+D:\programsfortests\allure\allure-2.24.1\bin\allure.bat generate allure-results\ --clean
+потом в аллюр-репорт открыть индекс.хтмл и открыть в хроме
+
+
+
+
+ЗАПУСК В ВИРТУАЛКЕ (http://192.168.244.128): открываем виртуалку, запускаем опенкарт docker-compose up -d
+на хосте запускаем из термиеналаpytest tests/test_find_elements.py --executor="192.168.244.128" --log_level=INFO  --remote=True 
+
+по умолчанию будут в хроме. можно добавить  --browser="firefox"
+на виртуалке можно открыть селеноид http://localhost:8080/
+
+после прогона тестов можно также открыть аллюр репорт
 D:\programsfortests\allure\allure-2.24.1\bin\allure.bat generate allure-results\ --clean
 потом в аллюр-репорт открыть индекс.хтмл и открыть в хроме
--- conftest.py
+++ conftest.py
@@ -6,6 +6,7 @@ from selenium.webdriver.edge.service import Service as EdgeService
 import os.path
 import logging
 import datetime
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
@@ -14,19 +15,24 @@ def pytest_addoption(parser):
     parser.addoption("--headless", action="store_true", help="headless режим. только в False(по умолчанию) или True")
     parser.addoption("--url", help="URL")
     parser.addoption("--log_level", action="store", default="INFO")
+    parser.addoption("--remote", default=False)
+    parser.addoption("--executor", default="localhost")
 
 
 @pytest.fixture
 def base_url(request):
-    return request.config.getoption("--url")
+    executor = request.config.getoption("--executor")
+    url = f"http://{executor}"
+    return url
 
 
 @pytest.fixture()
 def browser(request):
-    browser_name = request.config.getoption("--browser")
+    browser = request.config.getoption("--browser")
     drivers_folder = request.config.getoption("--drivers_folder")
     headless = request.config.getoption("--headless")
-    url = request.config.getoption("--url")
+    remote = request.config.getoption("--remote")
+    executor = request.config.getoption("--executor")
 
     log_level = request.config.getoption("--log_level")
     logger = logging.getLogger(request.node.name)
@@ -34,43 +40,60 @@ def browser(request):
     file_handler.setFormatter(logging.Formatter('%(levelname)s %(message)s'))
     logger.addHandler(file_handler)
     logger.setLevel(level=log_level)
-
     logger.info("===> Test %s started at %s" % (request.node.name, datetime.datetime.now()))
 
-    driver = None
-    if browser_name == "chrome":
-        options = webdriver.ChromeOptions()
-        if headless:
-            options.add_argument('--headless=new')
-        driver = webdriver.Chrome()
+    if not remote:
+        driver = None
+        if browser == "chrome":
+            options = webdriver.ChromeOptions()
+            if headless:
+                options.add_argument('--headless=new')
+            driver = webdriver.Chrome()
 
-    elif browser_name == "firefox":
-        options = webdriver.FirefoxOptions()
-        if headless:
-            options.add_argument('--headless=new')
-        service = FirefoxService(executable_path=os.path.join(drivers_folder, "chromedriver"))
-        driver = webdriver.Firefox(service=service, options=options)
+        elif browser == "firefox":
+            options = webdriver.FirefoxOptions()
+            if headless:
+                options.add_argument('--headless=new')
+            service = FirefoxService(executable_path=os.path.join(drivers_folder, "chromedriver"))
+            driver = webdriver.Firefox(service=service, options=options)
 
-    elif browser_name == "edge":
-        options = webdriver.EdgeOptions()
-        if headless:
-            options.add_argument('--headless=new')
-        service = EdgeService(executable_path=os.path.join(drivers_folder, "msedgedriver"))
-        driver = webdriver.Edge(service=service, options=options)
+        elif browser == "edge":
+            options = webdriver.EdgeOptions()
+            if headless:
+                options.add_argument('--headless=new')
+            service = EdgeService(executable_path=os.path.join(drivers_folder, "msedgedriver"))
+            driver = webdriver.Edge(service=service, options=options)
 
-    elif browser_name == "opera":
-        options = webdriver.ChromeOptions()
-        if headless:
-            options.add_argument('--headless=new')
-        service = ChromeService(executable_path=os.path.join(drivers_folder, "operadriver"))
-        driver = webdriver.Chrome(service=service, options=options)
+        elif browser == "opera":
+            options = webdriver.ChromeOptions()
+            if headless:
+                options.add_argument('--headless=new')
+            service = ChromeService(executable_path=os.path.join(drivers_folder, "operadriver"))
+            driver = webdriver.Chrome(service=service, options=options)
+
+    else:
+        capabilities = {
+            "browserName": browser,
+            "acceptInsecureCerts": True,
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            },
+        }
+        options = Options()
+        for k, v in capabilities.items():
+            options.set_capability(k, v)
+        driver = webdriver.Remote(
+            command_executor=f"http://{executor}:4444/wd/hub",
+            options=options
+        )
 
     driver.log_level = log_level
     driver.logger = logger
     driver.test_name = request.node.name
 
     driver.maximize_window()
-    driver.get(url)
+    driver.get(f"http://{executor}")
     yield driver
     driver.quit
     logger.info("===> Test %s finished at %s" % (request.node.name, datetime.datetime.now()))
